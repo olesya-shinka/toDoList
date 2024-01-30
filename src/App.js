0683d8ec47534799dba959ab8d35c5a110a01d2f
Olesya <os1124856@gmail.com>
2024-01-30 10:53:35 +0500

    структура

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="add todo" />
+      <button type="button" className="add-button">
+        add
+      </button>
+      <ul className="list-todo">
+        <li className="list-todo-item">to do</li>
+      </ul>
     </div>
   );
 }
