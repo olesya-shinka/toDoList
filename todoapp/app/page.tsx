d7db6dd7c225feab26e565c0f913c7dd12f6a4af
Olesya <os1124856@gmail.com>
2024-01-31 18:09:16 +0500

    add content

--- todoapp/app/page.tsx
+++ todoapp/app/page.tsx
@@ -1,5 +1,7 @@
 import Todos from "./components/Todos/Todos";
+import { useGlobalState } from "./context/GlobalProvider";
 
 export default function Home() {
+  const { todos } = useGlobalState();
   return <Todos />;
 }
